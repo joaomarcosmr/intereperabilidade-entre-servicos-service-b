d798941c755d7c031ac821790d6266c327bd2622
joao.rosa <joao.rosa@portobello.com.br>
2025-10-24 19:36:15 -0300

    feat: added swagger title config

--- src/main/java/com/challenge/geosapiens/service_b/application/config/OpenApiConfig.java
+++ src/main/java/com/challenge/geosapiens/service_b/application/config/OpenApiConfig.java
@@ -1,14 +1,10 @@
 package com.challenge.geosapiens.service_b.application.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,21 +12,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Service B API - GeoSapiens Challenge")
-                        .version("1.0.0")
-                        .description("API for managing users and orders in Service B")
-                        .contact(new Contact()
-                                .name("GeoSapiens Team")
-                                .email("contact@geosapiens.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
-                .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8081")
-                                .description("Local Server")
-                ));
+                .info(new Info().title("Service B API - GeoSapiens Challenge"));
     }
 }
 
